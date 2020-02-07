9a6b3542753da73d4e62241c77cb4e6a84d7ccf9
Jerinji2016 <jerinji2016@gmail.com>
2020-02-07 18:24:32 +0530

    check-commit

--- q-exam/script.js
+++ q-exam/script.js
@@ -9,3 +9,12 @@ function findAns() {
         }
     }
 }
+
+//Check and add
+function add(arr, name) {
+  const { length } = arr;
+  const id = length + 1;
+  const found = arr.some(el => el.username === name);
+  if (!found) arr.push({ id, username: name });
+  return arr;
+}
