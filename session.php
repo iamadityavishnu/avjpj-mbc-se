32b6b75ea2afcd04a5e12b0fc35d9ae6d9475052
Jerinji2016 <jerinji2016@gmail.com>
2020-03-21 22:47:36 +0530

    session db-root path updated

--- session.php
+++ session.php
@@ -3,11 +3,16 @@
 
     session_start();
 
-    $user_check = $_SESSION['user-id'];
-    $ses_sql = mysql_query("SELECT user_id FROM student_list WHERE user_id='$user_check'");
+    $user_name = $_SESSION['user-name'];
+    $user_id = $_SESSION['user-id'];
+    $ses_sql = mysql_query("SELECT user_id FROM student_list WHERE user_id='$user_id'");
     $row = mysql_fetch_array($ses_sql);
 
+    echo $_SERVER['DOCUMENT ROOT'];
+
     if(!isset($row[0])) {
-        header('Location: index.html');
+        header('Location: '.$_SERVER['DOCUMENT ROOT'].'\\avjpj-mbc-se\\');
     }
+    // else
+    //     echo "<script> console.log('Session check successfull') </script>";
 ?>
